0749e4d202dd4e42735c50ee1e59664b535029b1
Belac44 <belaccc44@gmail.com>
2023-02-21 21:24:32 +0300

    Added tests

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -1,6 +1,8 @@
 # python3
 
 from collections import namedtuple
+import glob
+import os
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
@@ -16,22 +18,41 @@ def find_mismatch(text):
             
 
         if next in ")]}":
-            if next == "}" and opening_brackets_stack[-1] == "{":
-                opening_brackets_stack.pop()
-                
-            elif next == ")" and opening_brackets_stack[-1] == "(":
-                opening_brackets_stack.pop()
-                
-            elif next == "]" and opening_brackets_stack[-1] == "[":
-                opening_brackets_stack.pop()
-                
+            if len(opening_brackets_stack) > 0:
+                if next == "}" and opening_brackets_stack[-1] == "{":
+                    opening_brackets_stack.pop()
+                    
+                elif next == ")" and opening_brackets_stack[-1] == "(":
+                    opening_brackets_stack.pop()
+                    
+                elif next == "]" and opening_brackets_stack[-1] == "[":
+                    opening_brackets_stack.pop()
+                    
+                else:
+                    return f"{i+1}"
             else:
                 return f"{i+1}"
-    
     if len(opening_brackets_stack) == 0:
         return "Success"
     else:
-        return f"{len(text)}"
+        return f"{len(text.strip())}"
+
+def test():
+    files = os.listdir('./tests')
+    length = len(files)
+    for i in range(0, length-1, 2):
+        with open(f'./tests/{files[i]}') as file:
+            content = file.read()
+            feedback = find_mismatch(content)
+            with open(f"./tests/{files[i+1]}") as data:
+                label = data.read()
+                if label.strip() == feedback:
+                    print(f"Input:{content} Output:{feedback}  True value:{label}")
+                else:
+                    print(f"Broken with {content} Label:{label} Output:{feedback}")
+                    print(f"At pos {i}: Total: {length}")
+                    break
+
 
 
 def main():
@@ -41,4 +62,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    test()
