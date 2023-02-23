1d711b9b66a40f7549023c723f4c3d2721aeaff4
Belac44 <belaccc44@gmail.com>
2023-02-23 12:57:28 +0300

    Implemeting a fster Tree height algorithim

--- week1_basic_data_structures/2_tree_height/tree_height.py
+++ week1_basic_data_structures/2_tree_height/tree_height.py
@@ -6,15 +6,16 @@ import threading
 
 def compute_height(n, parents):
     # Replace this code with a faster implementation
-    max_height = 0
-    for vertex in range(n):
-        height = 0
-        current = vertex
-        while current != -1:
-            height += 1
-            current = parents[current]
-        max_height = max(max_height, height)
-    return max_height
+    # max_height = 0
+    # for vertex in range(n):
+    #     height = 0
+    #     current = vertex
+    #     while current != -1:
+    #         height += 1
+    #         current = parents[current]
+    #     max_height = max(max_height, height)
+    # return max_height
+    head = parents.index(-1)
 
 
 def main():
